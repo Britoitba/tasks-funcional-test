617ad0a993c090dc513c69d0fa303a220ecf6ab5
Francisco Nascimento <felipe.ryo@hotmail.com>
2022-11-05 16:28:37 -0300

    Finalizando

--- src/test/java/br/org/brito/tests/TasksTest.java
+++ src/test/java/br/org/brito/tests/TasksTest.java
@@ -2,22 +2,27 @@ package br.org.brito.tests;
 
 
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import java.util.concurrent.TimeUnit;
 
 public class TasksTest {
+
+    WebDriver driver = new ChromeDriver(getOptions());
     String name;
-    WebDriver driver = new ChromeDriver();
 
     @Test
-    public void shouldAddTodo(){
-        name = new Faker().name().firstName();
+    public void shouldAddTodo() {
         driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        name = new Faker().name().firstName();
         addTask(name, true, true);
         Assertions.assertEquals(returnWebElement("//p[@id='message']").getText(),  "Success!");
         driver.quit();
@@ -25,8 +30,9 @@ public class TasksTest {
 
     @Test
     public void shouldNotAddTodoWithoutDescription() {
-        name = new Faker().name().firstName();
         driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        name = new Faker().name().firstName();
         addTask(name, false, true);
         Assertions.assertEquals(returnWebElement("//p[@id='message']").getText(),"Fill the task description");
         driver.quit();
@@ -34,8 +40,9 @@ public class TasksTest {
 
     @Test
     public void shouldNotAddTodoWithoutData() {
-        name = new Faker().name().firstName();
         driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        name = new Faker().name().firstName();
         addTask(name, true, false);
         Assertions.assertEquals(returnWebElement("//p[@id='message']").getText(),"Fill the due date");
         driver.quit();
@@ -43,24 +50,26 @@ public class TasksTest {
 
     @Test
     public void shouldNotAddTodoWithoutDescriptionAndData() {
-        name = new Faker().name().firstName();
         driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        name = new Faker().name().firstName();
         addTask(name, false, false);
         Assertions.assertEquals(returnWebElement("//p[@id='message']").getText(),"Fill the task description");
         driver.quit();
     }
 
     @Test
-    public void shouldDeleteTask(){
-        name = new Faker().name().firstName();
+    public void shouldDeleteTask() {
         driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        name = new Faker().name().firstName();
         addTask(name, true, true);
         returnWebElement("//td[text()='"+ name +"']/following-sibling::td[2]/a[text()='Remove']").click();
         driver.quit();
     }
 
 
-    public void addTask(String taskName, Boolean description, Boolean data){
+    public void addTask(String taskName, Boolean description, Boolean data) {
         returnWebElement("//a[@id='addTodo']").click();
         if(description) {
             returnWebElement("//input[@id='task']").sendKeys(taskName);
@@ -71,9 +80,18 @@ public class TasksTest {
         returnWebElement("//input[@id='saveButton']").click();
     }
 
-    public WebElement returnWebElement(String elementName){
+    public WebElement returnWebElement(String elementName) {
         WebElement element = driver.findElement(By.xpath(elementName));
         return element;
+    }
+
+    public ChromeOptions getOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        options.setAcceptInsecureCerts(true);
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        return options;
     }
 
 }
